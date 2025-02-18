1041a80c9d3d77157fea7dd4f7c76004cbb12218
stefancourteaux <stefancourteaux@icloud.com>
2025-02-18 18:50:39 +0100

    Les 2025 02 18

---
+++ 2025-02-18/Storefront/Storefront.Api/Contracts/CustomerRequestContract.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Storefront.Api.Contracts;
+
+public class CustomerRequestContract
+{
+    [MaxLength(30)]
+    public string FirstName { get; set; }
+    [MaxLength(30)]
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    [EmailAddress]
+    public string Email { get; set; }
+    [MaxLength(20)]
+    public string? TaxIdentificationNumber { get; set; }
+    [MaxLength(50)]
+    public string Addressline1 { get; set; }
+    [MaxLength(50)]
+    public string Addressline2 { get; set; }
+    [MaxLength(50)]
+    public string? Addressline3 { get; set; }
+    [MaxLength(20)]
+    public string Country { get; set; }
+}
---
+++ 2025-02-18/Storefront/Storefront.Api/Program.cs
@@ -0,0 +1,25 @@
+using Storefront.Api.Repositories;
+
+namespace Storefront.Api;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddSingleton<ICustomerRepository, CustomerRepository>();
+        builder.Services.AddControllers();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+
+        app.UseHttpsRedirection();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
